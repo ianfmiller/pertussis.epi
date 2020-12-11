550b4c93045cd70022886bac58daf69c3d438aac
ifmiller <ifmiller@princeton.edu>
2020-12-11 17:52:39 -0500

    fixing rds reading

--- cluster/code/parameter.sweep.final.R
+++ cluster/code/parameter.sweep.final.R
@@ -11,9 +11,9 @@ start.job.index<-1 #first index parameter set to analyze
 
 ### set region, smoothing window, vaccine era, model
 loc<-"US" #loc is the region or vector of regions to analyze NEED TO MAKE SURE THIS WORKS FOR DC, NY, AND NYC
-model<-"test.stoch"
-subset.data<-"wP" #vaccine era to subset. options are "all" "wP" and "aP"
-smooth.interval<-"four.weeks" #time period over which to smooth data, options are "two.weeks" "four.weeks" and "none"
+model<-"none.equal"
+subset.data<-"all" #vaccine era to subset. options are "all" "wP" and "aP"
+smooth.interval<-"two.weeks" #time period over which to smooth data, options are "two.weeks" "four.weeks" and "none"
 job.name<-paste0(loc,".",model,".",subset.data,".",smooth.interval)
 
 ### set directories for reading files to be re-written and run###
@@ -63,6 +63,7 @@ foreach(i=0:(jobs.per.node-1), .inorder=F, .combine = "rbind") %dorng% {
   lhs.samp<-job.set[job.index]
   setwd(out.dir)
   m1<-readRDS(paste0(model,".",loc,".",subset.data,".",smooth.interval,".iter",lhs.samp,".mid.mif.RDS"))
+  m1<-m1$mif
   continue(m1,Nmif=650)->m2
   print(paste0("i = ",i+start.job.index," mif complete"))
   ll <- replicate(n=10,logLik(pfilter(m2,Np=1000)))
