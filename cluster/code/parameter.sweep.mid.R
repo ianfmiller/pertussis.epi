7abd19251eff3dedfa74d2bcc655ab54f43f7ff3
ifmiller <ifmiller@princeton.edu>
2020-12-31 12:56:45 -0500

    longer mif2, faster cooling, hopefully last tweak before trying other models

--- cluster/code/parameter.sweep.mid.R
+++ cluster/code/parameter.sweep.mid.R
@@ -64,7 +64,7 @@ foreach(i=0:(jobs.per.node-1), .inorder=F, .combine = "rbind") %dorng% {
   setwd(out.dir)
   m1<-readRDS(paste0(model,".",loc,".",subset.data,".",smooth.interval,".iter",lhs.samp,".initial.mif.RDS"))
   m1<-m1$mif
-  continue(m1,Nmif=50)->m2
+  continue(m1,Nmif=75)->m2
   print(paste0("i = ",i+start.job.index," mif complete"))
   ll <- replicate(n=10,logLik(pfilter(m2,Np=1000)))
   print(paste("finished i =",i+start.job.index))
