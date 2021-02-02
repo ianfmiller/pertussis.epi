ecc34d96dc27cdb889a0bfb8a005143b5e3815c3
ifmiller <ifmiller@princeton.edu>
2021-02-02 16:42:50 -0500

    changing np for ll computation to 10000; changing nmif from 100 to 10 for initial, 75 to 10 for mid/final for test

--- cluster/code/parameter.sweep.final.R
+++ cluster/code/parameter.sweep.final.R
@@ -56,9 +56,9 @@ foreach(i=0:(jobs.per.node-1), .inorder=F, .combine = "rbind") %dorng% {
   setwd(out.dir)
   m1<-readRDS(paste0(model,".",loc,".",subset.data,".",smooth.interval,".iter",lhs.samp,".mid.mif.RDS"))
   m1<-m1$mif
-  continue(m1,Nmif=75)->m2
+  continue(m1,Nmif=10)->m2
   print(paste0("i = ",i+start.job.index," mif complete"))
-  ll <- replicate(n=10,logLik(pfilter(m2,Np=1000)))
+  ll <- replicate(n=10,logLik(pfilter(m2,Np=10000)))
   print(paste("finished i =",i+start.job.index))
   m2<-list(mif=m2,ll=logmeanexp(ll,se=TRUE))
   setwd(out.dir)
