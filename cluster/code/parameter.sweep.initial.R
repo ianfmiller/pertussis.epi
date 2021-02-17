3611300dcad80a25ca92afa0a60eb771a2ef29d8
ifmiller <ifmiller@princeton.edu>
2021-02-17 12:28:23 -0500

    compromising on particles for ll filter, upping nmif, cutting run time

--- cluster/code/parameter.sweep.initial.R
+++ cluster/code/parameter.sweep.initial.R
@@ -50,7 +50,7 @@ foreach(i=0:(jobs.per.node-1), .inorder=F, .combine = "rbind") %dorng% {
   params<-params.mat[job.index,]
   m2<-mif2(m1,Nmif=25,params=params,rw.sd=rw.sd,cooling.fraction.50=0.25, Np=1000,cooling.type="geometric")
   print(paste0("i = ",i+start.job.index," mif complete; time = ",Sys.time()))
-  ll <- replicate(n=10,logLik(pfilter(m2,Np=10000)))
+  ll <- replicate(n=10,logLik(pfilter(m2,Np=5000)))
   print(paste("finished i =",i+start.job.index,"; time = ",Sys.time()))
   m2<-list(mif=m2,ll=logmeanexp(ll,se=TRUE))
   setwd(out.dir) 
