1e4a4f39827881983b57ce3602a64ab48a3e18ff
ifmiller <ifmiller@princeton.edu>
2021-03-11 11:27:03 -0500

    add final model

--- cluster/code/lhs.gen.R
+++ cluster/code/lhs.gen.R
@@ -972,6 +972,66 @@ if (file.exists(LHS.file.name))
     saveRDS(LHS,file=LHS.file.name)
   }
   
+  if (model=="waning.same.protection.same.symptoms.same.transmission.same")
+  {
+    lower.param.bounds<-c(
+      Vn_wane_rate = 1/(365*1),
+      V_wane_rate = 1/(365*1), 
+      Vn_fail_rate=0, 
+      Vn_symptom_rate=0,
+      V_fail_rate=0, 
+      V_symptom_rate=0, 
+      naive_symptom_rate=0,
+      beta0 = 1e-3, 
+      beta1 = 0.0,
+      beta_mod_An = 0.0, 
+      beta_mod_Av = 0.0, 
+      beta_mod_A = 0.0,
+      rho=1e-9,
+      sigmaSE=0,
+      lag=0,
+      S_0=0.0,
+      E_0=0.0,
+      En_0=0.0,
+      Ev_0=0.0,
+      I_0=0.0,
+      A_0=0.0,
+      An_0=0.0,
+      Av_0=0.0,
+      Vn_0=0.0,
+      V_0=0.0)
+    
+    upper.param.bounds<-c(
+      Vn_wane_rate = 1/(365*500),
+      V_wane_rate = 1/(365*500), 
+      Vn_fail_rate=1, 
+      Vn_symptom_rate=1,
+      V_fail_rate=1, 
+      V_symptom_rate=1, 
+      naive_symptom_rate=1,
+      beta0 = 1, 
+      beta1 = 1,
+      beta_mod_An = 1, 
+      beta_mod_Av = 1, 
+      beta_mod_A = 1,
+      rho=1,
+      sigmaSE=0.25,
+      lag=1,
+      S_0=1,
+      E_0=.1,
+      En_0=.1,
+      Ev_0=.1,
+      I_0=.1,
+      A_0=.1,
+      An_0=.1,
+      Av_0=.1,
+      Vn_0=1,
+      V_0=1)
+    
+    LHS<-sobolDesign(lower=lower.param.bounds,upper=upper.param.bounds,nseq=n.points)
+    saveRDS(LHS,file=LHS.file.name)
+  }
+  
 }
 setwd(base.dir)
 
