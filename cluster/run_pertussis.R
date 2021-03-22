3e48284fd27c3f7b8590573f6f4637735a6d0cf0
ifmiller <ifmiller@princeton.edu>
2021-03-22 15:56:21 -0400

    fix batch script names

--- cluster/run_pertussis.R
+++ cluster/run_pertussis.R
@@ -21,6 +21,23 @@ models<-c(
           "waning.same.protection.diff.symptoms.same.transmission.same",
           "waning.diff.protection.same.symptoms.same.transmission.same",
           "waning.same.protection.same.symptoms.same.transmission.same")
+
+model.abbrevs<-c("dddd",
+                 "sddd",
+                 "dsdd",
+                 "ddsd",
+                 "ddds",
+                 "ssdd",
+                 "sdsd",
+                 "sdds",
+                 "dssd",
+                 "dsds",
+                 "ddss",
+                 "sssd",
+                 "ssds",
+                 "sdss",
+                 "dsss",
+                 "ssss")
 subset.data<-"all" #vaccine era to subset. options are "all" "wP" and "aP"
 smooth.interval<-"two.weeks"  #time period over which to smooth data, options are "two.weeks" "four.weeks" and "none"
 for(model in models)
@@ -33,6 +50,7 @@ for(loc in locs) #add nested loops if needed for other models, subsets, etc.
     analysis.setup.lines[min(grep('smooth.interval<-',analysis.setup.lines))]<-paste0("smooth.interval<-",'"',smooth.interval,'"')
   
     job.name<-paste0(loc,".",model,".",subset.data,".",smooth.interval)
+    job.name.abrev<-paste0(loc,".",model.abbrevs[which(models==model)],".",subset.data,".",smooth.interval)
     setwd("~/pertussis")
     if (!dir.exists(job.name)) {dir.create(job.name)}
     setwd(job.name)
@@ -41,11 +59,11 @@ for(loc in locs) #add nested loops if needed for other models, subsets, etc.
     setwd(paste0("~/pertussis/",job.name))
     
     cmd<-paste0("jid1=$(sbatch ",paste0(job.name,".initial.q);"),
-                " jid2=$(sbatch --dependency=afterany:${jid1##* } ",paste0(job.name,".comp.initial.q);"),
-                " jid3=$(sbatch --dependency=afterany:${jid2##* } ",paste0(job.name,".mid.q);"),
-                " jid4=$(sbatch --dependency=afterany:${jid3##* } ",paste0(job.name,".comp.mid.q);"),
-                " jid5=$(sbatch --dependency=afterany:${jid4##* } ",paste0(job.name,".final.q);"),
-                " sbatch --dependency=afterany:${jid5##* } ",paste0(job.name,".comp.final.q"))
+                " jid2=$(sbatch --dependency=afterany:${jid1##* } ",paste0(job.name.abrev,".comp.initial.q);"),
+                " jid3=$(sbatch --dependency=afterany:${jid2##* } ",paste0(job.name.abrev,".mid.q);"),
+                " jid4=$(sbatch --dependency=afterany:${jid3##* } ",paste0(job.name.abrev,".comp.mid.q);"),
+                " jid5=$(sbatch --dependency=afterany:${jid4##* } ",paste0(job.name.abrev,".final.q);"),
+                " sbatch --dependency=afterany:${jid5##* } ",paste0(job.name.abrev,".comp.final.q"))
     system(cmd)
 }
 }
