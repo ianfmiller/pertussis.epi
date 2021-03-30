064c2f4314f1ac53ac64dc9f7283a140fe34e8d4
ifmiller <ifmiller@princeton.edu>
2021-03-30 10:59:13 -0400

    extend run times

--- cluster/code/parameter.sweep.initial.R
+++ cluster/code/parameter.sweep.initial.R
@@ -70,7 +70,7 @@ foreach(i=0:(jobs.per.node-1), .inorder=F, .combine = "rbind") %dorng% {
   m2<-mif2(m1,Nmif=25,params=params,rw.sd=rw.sd,cooling.fraction.50=0.25, Np=1000,cooling.type="geometric")
   print(paste0("i = ",i+start.job.index," mif complete; time = ",Sys.time()))
   ### get liklihood with check to kill slow calculation of extremely low liklihood 
-  ll <- tryCatch(withTimeout(replicate(n=10,logLik(pfilter(m2,Np=5000))),timeout = 60*60*15),error=function(e) {ll.tmp<- rep(-888e10,times=10)})
+  ll <- tryCatch(withTimeout(replicate(n=10,logLik(pfilter(m2,Np=5000))),timeout = 60*60*20),error=function(e) {ll.tmp<- rep(-888e10,times=10)})
   print(paste("finished i =",i+start.job.index,"; time = ",Sys.time()))
   m2<-list(mif=m2,ll=logmeanexp(ll,se=TRUE))
   setwd(out.dir) 
