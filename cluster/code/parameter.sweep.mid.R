b7e56df5cecb661a177dd5d51a7181b309b1a95a
ifmiller <ifmiller@princeton.edu>
2021-06-28 09:02:03 -0600

    last attempt at getting pomp to estimate parameters that recapitulate dynamics before trying deterministic approach

--- cluster/code/parameter.sweep.mid.R
+++ cluster/code/parameter.sweep.mid.R
@@ -55,9 +55,9 @@ foreach(i=0:(jobs.per.node-1), .inorder=F, .combine = "rbind") %dorng% {
   m1<-readRDS(paste0(model,".",loc,".",subset.data,".",smooth.interval,".iter",lhs.samp,".initial.mif.RDS"))
   m1<-m1$mif
   #continue(m1,Nmif=50)->m2 # for two.week smooth interval
-  continue(m1,Nmif=150)->m2 # for four.week smooth interval
+  continue(m1,Nmif=10)->m2 # for four.week smooth interval
   print(paste0("i = ",i+start.job.index," mif complete; time = ",Sys.time()))
-  ll <- replicate(n=10,logLik(pfilter(m2,Np=5000)))
+  ll <- replicate(n=10,logLik(pfilter(m2,Np=10000)))
   print(paste("finished i =",i+start.job.index,"; time = ",Sys.time()))
   m2<-list(mif=m2,ll=logmeanexp(ll,se=TRUE))
   setwd(out.dir)
